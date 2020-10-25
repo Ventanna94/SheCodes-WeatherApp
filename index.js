65afd7397d6db4f283f5cce1b5f86c227c23fdb8
Anna V <71232941+Ventanna94@users.noreply.github.com>
2020-10-25 09:04:39 +0000

    Uploaded files from hard drive

---
+++ index.js
@@ -0,0 +1,56 @@
+let currentTime = new Date();
+let date = currentTime.getDate();
+let hour = currentTime.getHours();
+if (hour < 10) {
+    hour = `0${hour}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`
+} 
+
+let months =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let month = months[currentTime.getMonth()];
+
+let h2 = document.querySelector("h2");
+h2.innerHTML = `${date} ${month} | ${hour}:${minutes}`
+
+function showWeather(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#description").innerHTML = response.data.weather[0].main;
+}
+
+function search(city) {
+    let apiKey = "2547a6a5f6c6d363e76bf41f4f629620";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#search-city-input").value;
+    search(city);
+} 
+
+function searchLocation (position) {
+let apiKey = "2547a6a5f6c6d363e76bf41f4f629620";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(showWeather)}
+
+
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentButton = document.querySelector("#current");
+currentButton.addEventListener("click", getCurrentLocation);
+
+search("Milan");
+
