051668e4a00ba0508e9e20166137403f77267e7e
Anna V <71232941+Ventanna94@users.noreply.github.com>
2020-11-01 10:12:58 +0000

    Creating function to display forecast

--- index.js
+++ index.js
@@ -27,10 +27,15 @@ function showWeather(response) {
     document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
 }
 
+function displayForecast()
+
 function search(city) {
     let apiKey = "2547a6a5f6c6d363e76bf41f4f629620";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showWeather);
+
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
